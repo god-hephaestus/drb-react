659823ffb3c72c3a609936c4c6dbf1134fc0240c
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-12-10 09:42:56 +0300

    import and apply fonts

--- src/app/page.js
+++ src/app/page.js
@@ -18,22 +18,24 @@ export default function Home() {
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/50 via-black/30 to-black/70"></div>
 
         {/* Content Overlay */}
-        <div className="relative z-10 flex flex-col items-center text-center text-white space-y-4">
+        <div className="relative z-10 flex flex-col items-center text-center text-white gap-4 lg:gap-6">
           {/* Logo */}
           <Image
             aria-hidden
             src="/doctor-b-logo.webp"
             alt="Doctor B Logo"
-            width={120}
-            height={75}
+            width={180}
+            height={155}
+            className="pt-[3%]"
           />
 
           {/* Tagline */}
-          <p className="text-xl font-light">Discover</p>
-          <p className="text-xl font-light">The Expertise and Artistry of</p>
+          <p className="text-2xl  font-light monument-font monument-heading">
+            Discover <br /> The Expertise and Artistry of
+          </p>
 
           {/* Hero Title */}
-          <h1 className="text-6xl md:text-7xl font-bold tracking-wide drop-shadow-lg">
+          <h1 className="text-6xl md:text-7xl font-bold tracking-wide drop-shadow-lg monument-heading">
             Doctor B
           </h1>
 
@@ -44,12 +46,13 @@ export default function Home() {
             alt="Doctor B Ratings"
             width={350}
             height={75}
+            className=""
           />
 
           {/* Call to Action */}
           <button
             type="button"
-            className="mt-6 text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-10 py-3 shadow-lg"
+            className="mt-6 text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-10 py-3 shadow-lg monument-heading"
           >
             Get a Quote
           </button>
