824c9c0580d65a30fb86745606a61d3684d0ee5f
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-12-17 10:11:55 +0300

    Update Advantages.js

--- src/components/Advantages.js
+++ src/components/Advantages.js
@@ -8,74 +8,59 @@ export default function Advantages() {
           Why Choose Dr B
         </h3>
         <div className="flex justify-center items-center flex-wrap">
-          <div className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4">
-            <Image
-              src={"/icons/adv-1.svg"}
-              alt="advantage icon"
-              width={64}
-              height={64}
-            />
-            <p className="text-lg font-semibold p-2 text-center">
-              30+ Years Of Experience
-            </p>
-          </div>
-          <div className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4">
-            <Image
-              src={"/icons/adv-2.svg"}
-              alt="advantage icon"
-              width={64}
-              height={64}
-            />
-            <p className="text-lg font-semibold p-2 text-center">
-              World-renowned Doctor
-            </p>
-          </div>
-          <div className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4">
-            <Image
-              src={"/icons/adv-3.svg"}
-              alt="advantage icon"
-              width={64}
-              height={64}
-            />
-            <p className="text-lg font-semibold p-2 text-center">
-              Golden Bistoury Awards
-            </p>
-          </div>
-          <div className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4">
-            <Image
-              src={"/icons/adv-4.svg"}
-              alt="advantage icon"
-              width={64}
-              height={64}
-            />
-            <p className="text-lg font-semibold p-2 text-center">
-              Top Medical Standards
-            </p>
-          </div>
-          <div className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4">
-            <Image
-              src={"/icons/adv-5.svg"}
-              alt="advantage icon"
-              width={64}
-              height={64}
-            />
-            <p className="text-lg font-semibold p-2 text-center">
-              Patented Special Techniques
-            </p>
-          </div>
-          <div className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4">
-            <Image
-              src={"/icons/adv-6.svg"}
-              alt="advantage icon"
-              width={64}
-              height={64}
-            />
-            <p className="text-lg font-semibold p-2 text-center">
-              The Best Medical Center in Turkey
-            </p>
-          </div>
+          {advantages.map((advantage, index) => (
+            <div
+              key={index}
+              className="lg:w-1/3 w-1/2 flex flex-col justify-center items-center py-4"
+            >
+              <div className="relative w-16 h-16">
+                <Image
+                  src={advantage.icon}
+                  alt={advantage.alt}
+                  fill
+                  className="object-contain"
+                />
+              </div>
+              <p className="text-lg font-semibold p-2 text-center">
+                {advantage.text}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 }
+
+const advantages = [
+  {
+    icon: "/icons/adv-1.svg",
+    alt: "advantage icon",
+    text: "30+ Years Of Experience",
+  },
+  {
+    icon: "/icons/adv-2.svg",
+    alt: "advantage icon",
+    text: "World-renowned Doctor",
+  },
+  {
+    icon: "/icons/adv-3.svg",
+    alt: "advantage icon",
+    text: "Golden Bistoury Awards",
+  },
+  {
+    icon: "/icons/adv-4.svg",
+    alt: "advantage icon",
+    text: "Top Medical Standards",
+  },
+  {
+    icon: "/icons/adv-5.svg",
+    alt: "advantage icon",
+    text: "Patented Special Techniques",
+  },
+  {
+    icon: "/icons/adv-6.svg",
+    alt: "advantage icon",
+    text: "The Best Medical Center in Turkey",
+  },
+];
