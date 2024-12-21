03c683164b6739998ff08ae5163e6b728845feec
Batuhan Şahin <111083432+god-hephaestus@users.noreply.github.com>
2024-12-21 11:24:03 +0300

    fix padding

--- src/components/tr/LanguageDropdown.js
+++ src/components/tr/LanguageDropdown.js
@@ -19,12 +19,15 @@ const LanguageDropdown = () => {
     // Dil değişimi ile ilgili işlem yapabilirsiniz
   };
 
-  const currentLangObj = languages.find((lang) => lang.code === currentLanguage);
+  const currentLangObj = languages.find(
+    (lang) => lang.code === currentLanguage
+  );
 
   return (
     <div className="absolute top-[10px] right-[10px] inline-block group">
       <button className="bg-[#c0a162] text-black px-[10px] py-[5px] border-0 cursor-pointer w-[120px] flex justify-between align-center text-[14px] h-[30px] transition-colors duration-300 ease-in-out group-hover:bg-[#c0a162]">
-        <Image className="object-cover"
+        <Image
+          className="object-cover"
           src={`${currentLangObj.flag}`}
           alt={`${currentLangObj.name} Flag`}
           width={20}
@@ -34,12 +37,14 @@ const LanguageDropdown = () => {
       </button>
       <ul className="hidden group-hover:block absolute top-[100%] left-0 w-[120px] bg-[#c0a162] b-[#c0a162] z-[1000] p-0 m-0">
         {languages.map((lang) => (
-          <li className="list-none m-[10px]" key={lang.code}>
-            <a className="flex align-center justify-between text-black no-underline text-[14px] gap-[10px] hover:bg-[#0a0a0a] hover:text-[#c0a162]"
-             href={lang.code === 'en' ? '/' : `/${lang.code}`}
+          <li className="list-none" key={lang.code}>
+            <a
+              className="p-[10px] flex align-center justify-between text-black no-underline text-[14px] gap-[10px] hover:bg-[#0a0a0a] hover:text-[#c0a162]"
+              href={lang.code === "en" ? "/" : `/${lang.code}`}
               onClick={() => handleLanguageChange(lang.code)}
             >
-              <Image className="object-cover"
+              <Image
+                className="object-cover"
                 src={`${lang.flag}`}
                 alt={`${lang.name} Flag`}
                 width={20}
